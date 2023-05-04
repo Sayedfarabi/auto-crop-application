563c8fb866af41bd8e0d22e9977ce5e66488cd89
Sayedfarabi <sayedfarabi6099@gmail.com>
2023-05-04 17:49:19 +0600

    banner section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import ImageCropper from './components/image-cropper/ImageCropper';
 import Navbar from './components/navbar/Navbar';
+import Banner from './components/banner/Banner';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
       <ImageCropper></ImageCropper>
     </>
   );
--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div>
-
-        </div>
+        <section className='py-12 md:py-16'>
+            <div className='text-center mt-12'>
+                <h1 className='text-2xl md:text-4xl font-semibold mb-2 text-gray-800'>Image <span className='text-lime-500'>Resizer</span> </h1>
+                <p className='text-xl md:text-2xl text-gray-600'>Easily resize images online for free.</p>
+            </div>
+        </section>
     );
 };
 
